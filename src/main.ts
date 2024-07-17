d8b34dbfe7825665285c1f93bade109784df98b4
Andrew Mason <github@andrew.coderonfire.com>
2024-07-17 16:33:22 +0100

    Add sytles

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,48 @@
+import "./main.css";
+
 console.log("Hello World!");
+
+/**
+ * # Core ideas
+ *
+ * - Animate defrag
+ * - Options
+ *  - HD size
+ *  - HD speed
+ *  - Theme
+ *  - Sound on/off
+ *  - Damage level
+ *
+ */
+
+enum Theme {
+  Win95 = "Win95",
+  Dos = "Dos",
+}
+
+interface DefragOptions {
+  hdSize: number;
+  hdSpeed: number;
+  theme: Theme;
+  sound: boolean;
+  damageLevel: number;
+}
+
+enum SectorState {
+  Used = "User",
+  Reading = "Reading",
+  Bad = "Bad",
+  Unused = "Unused",
+  Writing = "Writing",
+  Unmoved = "Unmoved",
+}
+
+const defaultOptions: DefragOptions = {
+  hdSize: 10,
+  hdSpeed: 7,
+  theme: Theme.Dos,
+  sound: true,
+  damageLevel: 3,
+};
+
+export { type DefragOptions, defaultOptions, SectorState };
--- www/main.js
+++ www/main.js
@@ -2,5 +2,21 @@
 (() => {
   // src/main.ts
   console.log("Hello World!");
+  var SectorState = /* @__PURE__ */ ((SectorState2) => {
+    SectorState2["Used"] = "User";
+    SectorState2["Reading"] = "Reading";
+    SectorState2["Bad"] = "Bad";
+    SectorState2["Unused"] = "Unused";
+    SectorState2["Writing"] = "Writing";
+    SectorState2["Unmoved"] = "Unmoved";
+    return SectorState2;
+  })(SectorState || {});
+  var defaultOptions = {
+    hdSize: 10,
+    hdSpeed: 7,
+    theme: "Dos" /* Dos */,
+    sound: true,
+    damageLevel: 3
+  };
 })();
 //# sourceMappingURL=main.js.map
